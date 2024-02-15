aad4e48661c943c5c402d5ac5787baff395df31c
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-02-15 16:44:56 -0500

    Added Project Card Component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,58 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import { HiOutlineBookmark } from 'react-icons/hi';
+import React from 'react';
+
+function ProjectCard({ projectDetail }) {
+  return (
+    <Card sx={{ borderRadius: 8, p: 1 }} elevation={3}>
+      <CardHeader
+        sx={{ mb: -3 }}
+        avatar={
+          <Avatar
+            src={projectDetail.company_logo_url}
+            alt={`${projectDetail.company_name}'s Logo`}
+          />
+        }
+        action={
+          <IconButton>
+            <HiOutlineBookmark style={{ color: 'black' }} />
+          </IconButton>
+        }
+        title={projectDetail.company_name}
+        titleTypographyProps={{ fontWeight: 500, fontSize: 24 }}
+      />
+      <CardContent>
+        <Typography variant='h4'>{projectDetail.title}</Typography>
+        <Typography noWrap color='text.secondary' variant='body2'>
+          Date posted: {projectDetail.date_posted} | Topics:{' '}
+          {projectDetail.topics.join(', ')}
+        </Typography>
+        <Typography sx={{ fontWeight: 'bold', mt: 2, fontSize: 16 }}>
+          Description:
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: 14,
+            mt: 1,
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+            display: '-webkit-box',
+            WebkitLineClamp: '5',
+            WebkitBoxOrient: 'vertical',
+          }}
+        >
+          {projectDetail.description}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ProjectCard;
