51f284289dbb4a96c707006e918c6b5fb7ab4c6e
kwan <kwan@kwan.hsd1.ma.comcast.net>
2024-02-15 18:17:18 -0500

    Test react

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ export default function Home() {
             Deploy <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            This is a random line .
           </p>
         </a>
       </div>
