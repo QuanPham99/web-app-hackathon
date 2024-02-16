aa3879af19ad1358d42efe3d52671d0e7977a42e
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-02-16 18:49:14 -0500

    Added page for accepted projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,12 @@
 import {
   Avatar,
+  Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
   IconButton,
+  Stack,
   Typography,
 } from '@mui/material';
 import { HiOutlineBookmark } from 'react-icons/hi';
@@ -11,7 +14,7 @@ import React from 'react';
 
 const descriptionMaxLine = 3;
 
-function ProjectCard({ projectDetail }) {
+function ProjectCard({ user, projectDetail }) {
   return (
     <Card sx={{ borderRadius: 8, p: 1 }} elevation={1}>
       <CardHeader
@@ -33,8 +36,10 @@ function ProjectCard({ projectDetail }) {
       <CardContent>
         <Typography variant='h4'>{projectDetail.title}</Typography>
         <Typography noWrap color='text.secondary' variant='body2'>
-          Date posted: {projectDetail.date_posted} | Topics:{' '}
-          {projectDetail.topics.join(', ')}
+          {user && user.role === 'prof' && projectDetail.status === 'accepted'
+            ? `Date accepted: ${projectDetail.date_accepted}`
+            : `Date posted: ${projectDetail.date_posted}`}{' '}
+          | Topics: {projectDetail.topics.join(', ')}
         </Typography>
         <Typography sx={{ fontWeight: 'bold', mt: 2, fontSize: 16 }}>
           Description:
@@ -52,6 +57,54 @@ function ProjectCard({ projectDetail }) {
         >
           {projectDetail.description}
         </Typography>
+
+        {user && user.role === 'prof' && (
+          <Box sx={{ marginTop: 2 }}>
+            <Stack spacing={1}>
+              {projectDetail.num_students && projectDetail.num_students > 0 ? (
+                <Typography sx={{ color: 'success.light' }}>
+                  Assigned to {projectDetail.num_students} students.
+                </Typography>
+              ) : (
+                <Typography sx={{ color: 'error.main' }}>
+                  No students assigned.
+                </Typography>
+              )}
+
+              <Stack
+                direction={{ xs: 'column', sm: 'row' }}
+                spacing={{ xs: 1, sm: 2, md: 4 }}
+              >
+                <Button
+                  variant='contained'
+                  sx={{
+                    textTransform: 'none',
+                    borderRadius: '24px',
+                    bgcolor: 'black',
+                    '&:hover': { bgcolor: 'rgb(80,80,80)' },
+                  }}
+                >
+                  Assign Students
+                </Button>
+                <Button
+                  variant='outlined'
+                  sx={{
+                    textTransform: 'none',
+                    borderRadius: '24px',
+                    color: 'black',
+                    borderColor: 'black',
+                    '&:hover': {
+                      borderColor: 'rgb(80,80,80)',
+                      bgcolor: 'rgb(232,232,232)',
+                    },
+                  }}
+                >
+                  Complete Project
+                </Button>
+              </Stack>
+            </Stack>
+          </Box>
+        )}
       </CardContent>
     </Card>
   );
