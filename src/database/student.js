48e3f86a16a469b67311c70ed4be62f13236f9c6
QuanPham99 <jimmy.pham310899@gmail.com>
2024-02-22 01:32:29 -0500

    api

---
+++ src/database/student.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+import client from './client';
+
+export const getStudent = async ({ username = 'posted '}) => {
+    try {
+        await client.connect();
+
+        const query = { username : username};
+        
+        const options = { }
+
+        const student = client
+            .db('User')
+            .collection("People")
+            .find(query, options);
+
+        const data = await student.toArray();
+        return { success: true, data: data};
+    } catch (error) {
+        return { success: false, error };
+    } finally {
+        await client.close();
+    }
+};
