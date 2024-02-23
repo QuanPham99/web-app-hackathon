adf8bcbbe6b52be1eb3b52168beae661f2420a1d
Minh Q. Le <77988736+lqm181@users.noreply.github.com>
2024-02-23 02:12:49 -0800

    Fixed legacy mui

--- src/app/student/page.js
+++ src/app/student/page.js
@@ -1,21 +1,6 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Avatar, Typography, Container, Grid, Paper } from '@material-ui/core';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: theme.spacing(4),
-  },
-  avatar: {
-    width: theme.spacing(12),
-    height: theme.spacing(12),
-    marginBottom: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(4),
-  },
-}));
+import { Avatar, Typography, Container, Grid, Paper } from '@mui/material';
 
 function ProfilePage() {
   const [data, setData] = useState(null);
@@ -23,25 +8,21 @@ function ProfilePage() {
 
   const fetchStudentProject = async (studentData) => {
     try {
-      // const body = {
-      //   _id: data._id
-      // }
-      // const res = await fetch('/api/students/project', {method: "GET", body: JSON.stringify({...body})});
-      // Construct query string with _id parameter
-      // console.log("Data ID " + studentData._id);
       const queryString = `?_id=${studentData._id}`;
       // Send GET request with query string
-      const res = await fetch(`/api/students/project${queryString}`, { method: "GET" });
+      const res = await fetch(`/api/students/project${queryString}`, {
+        method: 'GET',
+      });
       if (!res.ok) {
         throw Error('Unknown Error');
       } else {
         const jsonData = await res.json();
         setProjects(jsonData.data[0]);
-        }
-      } catch (error) {
-        console.error('Error fetching student project:', error);
       }
-  }
+    } catch (error) {
+      console.error('Error fetching student project:', error);
+    }
+  };
 
   const fetchStudentData = async () => {
     try {
@@ -63,31 +44,41 @@ function ProfilePage() {
     fetchStudentData();
   }, []);
 
-
   const classes = useStyles();
   return (
-
-    data && (<Container className={classes.root} maxWidth="md">
-      <Paper className={classes.paper}>
-        <Grid container spacing={3} alignItems="center" justifyContent="center">
-          <Grid item xs={12} md={4} align="center">
-            <Avatar alt="Profile Picture" src="profile.jpg" className={classes.avatar} />
-                <Typography variant="h6">{data.first_name} {data.last_name}</Typography>
-                {/* <Typography variant="subtitle1" color="textSecondary"></Typography> */}
+    data && (
+      <Container maxWidth='md'>
+        <Paper>
+          <Grid
+            container
+            spacing={3}
+            alignItems='center'
+            justifyContent='center'
+          >
+            <Grid item xs={12} md={4} align='center'>
+              <Avatar
+                alt='Profile Picture'
+                src='profile.jpg'
+                className={classes.avatar}
+              />
+              <Typography variant='h6'>
+                {data.first_name} {data.last_name}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography variant='body1'>Role: {data.role}</Typography>
+              <Typography variant='body1'>Email: {data.email}</Typography>
+              {project && (
+                <Typography variant='body1'>
+                  Current Project Title: {project.title}{' '}
+                </Typography>
+              )}
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={8}>
-            <Typography variant="body1">
-              {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et leo vel magna pulvinar suscipit. Vivamus sit amet enim ultrices, fermentum purus et, commodo velit. Integer interdum vehicula ligula ut luctus. */}
-            </Typography>
-            <Typography variant="body1">Role: {data.role}</Typography> 
-            <Typography variant="body1">Email: {data.email}</Typography> 
-            {project && 
-            (<Typography variant="body1">Current Project Title: {project.title} </Typography>)}
-          </Grid>
-        </Grid>
-      </Paper>
-    </Container>
-  ));
-};
+        </Paper>
+      </Container>
+    )
+  );
+}
 
 export default ProfilePage;
